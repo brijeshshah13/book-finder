74da4d2e96386a71a34e8625677b2ea0a6622c35
Brijesh Shah <brijeshjain13@gmail.com>
2021-04-24 21:09:17 +0530

    feat: initial commit

---
+++ app/src/main/java/in/tuls/bookfinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package `in`.tuls.bookfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
