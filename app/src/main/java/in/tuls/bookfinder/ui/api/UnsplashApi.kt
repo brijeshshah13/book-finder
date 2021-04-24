cb0abd8e0809dcbc77f96ec7d385c2c590a94d68
Brijesh Shah <brijeshjain13@gmail.com>
2021-04-25 02:14:23 +0530

    feat: add search api

---
+++ app/src/main/java/in/tuls/bookfinder/ui/api/UnsplashApi.kt
@@ -0,0 +1,19 @@
+package `in`.tuls.bookfinder.ui.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    companion object {
+        const val BASE_URL = "https://www.googleapis.com/"
+    }
+
+    @GET("books/v1/volumes")
+    suspend fun searchBooks(
+        @Query("q") query: String,
+        @Query("startIndex") startIndex: Int,
+        @Query("maxResults") maxResults: Int
+    )
+
+}
---
+++ app/src/main/java/in/tuls/bookfinder/ui/api/UnsplashResponse.kt
@@ -0,0 +1,7 @@
+package `in`.tuls.bookfinder.ui.api
+
+import `in`.tuls.bookfinder.ui.data.UnsplashPhoto
+
+data class UnsplashResponse(
+    val items: List<UnsplashPhoto>
+)
