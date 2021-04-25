3cfa01507a41a71a2121237cbf8348f76959aa9d
Brijesh Shah <brijeshjain13@gmail.com>
2021-04-26 00:31:32 +0530

    feat: integrate load state header & footer

--- app/src/main/java/in/tuls/bookfinder/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/in/tuls/bookfinder/ui/gallery/GalleryFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import com.example.searchapp.ui.gallery.UnsplashPhotoLoadStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,7 +27,10 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
-            recyclerView.adapter = adapter
+            recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+                footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
+            )
         }
 
         viewModel.books.observe(viewLifecycleOwner) {
